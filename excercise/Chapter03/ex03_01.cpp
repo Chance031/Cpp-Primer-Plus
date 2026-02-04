35a4b98703391baf9517484e670f1944c9664de4
aday031 <aday031@naver.com>
2026-02-04 23:51:11 +0900

    exercise

---
+++ excercise/Chapter03/ex03_01.cpp
@@ -0,0 +1,24 @@
+// Q. 키를 정수형 센티미터 단위로 묻고 그 값을 미터와 센티미터 단위로 변환하는 간단한 프로그램을 작성하라. 입력해야 할 곳을 사용자에게 지시하기 위해 밑줄 문자를 사용하고 환산 인수로 const 기호 상수를 사용하라.
+
+#include <iostream>
+
+int main()
+{
+	int height_cm;
+
+	const int cm_per_m = 100;
+
+	std::cout << "키를 입력하시오(센티미터 단위): ___\b\b\b";
+	if (!(std::cin >> height_cm))
+	{
+		std::cout << "숫자를 입력하십시오.";
+		return 1;
+	}
+
+	int meters = height_cm / cm_per_m;
+	int centimeters = height_cm % cm_per_m;
+
+	std::cout << "키는 " << meters << "미터 " << centimeters << "센티미터입니다." << std::endl;
+
+	return 0;
+}
