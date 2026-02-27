852a9608127f1469b138642b9818eb0dba3411e4
aday031 <aday031@naver.com>
2026-02-27 23:20:29 +0900

    temp

--- excercise/Chapter08/ex08_04.cpp
+++ excercise/Chapter08/ex08_04.cpp
@@ -2,18 +2,18 @@
 // [#include <iostream>]
 // [using namespace std;]
 // [#include <cstring>		// strlen(), strcpy()를 사용하기 위해]
-// [struct stringy
+// [struct stringy]
 // [{]
 // [	char* str;			// 문자열을 지시한다]
 // [	int ct;				// 문자열의 길이('\0'은 제외)]
-// [};
-// [
+// [};]
+// 
 // [// set(), show()의 원형과, show() 함수를 이 자리에 넣는다]
-// [int main()
+// [int main()]
 // [{]
 // [	stringy beany;]
 // [	char testing[] = "Reality isn't what it used to be.";]
-// [
+// 
 // [	set(beany, testing);]
 // [			// 첫 번째 매개변수는 참조이다,]
 // [			// testing의 사본을 저장할 공간을 대입한다,]
@@ -27,7 +27,73 @@
 // [	show(testing);		// testing 문자열을 한 번 출력한다]
 // [	show(testing, 3);	// testing 문자열을 세 번 출력한다]
 // [	show("Done!");]
-// [	return 0;
+// [	return 0;]
 // [}]
 // 요구하는 함수와 원형을 작성하여 이 뼈대를 완성하라. 디폴트 매개변수를 사용하는 두 개의 show() 함수를 사용해야 한다. 필요하면 const 매개변수를 사용하라.
 // set()은 new를 사용하여 주어진 문자열을 저장할 수 있을 만큼 충분한 공간을 대입해야 한다. 여기서 사용하는 기법은 클래스를 설계하고 구현할 때 사용하는 것과 비슷하다. (사용하는 컴파일러가 헤더 파일 이름을 변경하고, using 지시자의 삭제를 요구할 수도 있다.)
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct stringy
+{
+	char* str;
+	int ct;
+};
+
+void set(stringy&,const char*);
+void show(const stringy&, int n = 1);
+void show(const char*, int n = 1);
+
+int main()
+{
+	stringy beany;
+	char testing[] = "Reality isn't what it used to be.";
+
+	set(beany, testing);
+
+	show(beany);
+	show(beany, 2);
+	testing[0] = 'D';
+	testing[1] = 'u';
+	show(testing);
+	show(testing, 3);
+	show("Done!");
+	delete[] beany.str;
+	return 0;
+}
+
+void set(stringy& str,const char* ar)
+{
+	int i = 0;
+
+	while (ar[i] != '\0')
+	{
+		i++;
+	}
+
+	char* pStr = new char[i + 1];
+
+	str.str = pStr;
+	str.ct = i;
+
+	strcpy(str.str, ar);
+}
+
+void show(const stringy& str, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << str.str << std::endl;
+	}
+}
+
+void show(const char* str, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << str << std::endl;
+	}
+}
