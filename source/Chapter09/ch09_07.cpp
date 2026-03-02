b04fddb86ce46e12e0ab3efc3d2d6065f2b54eea
aday031 <aday031@naver.com>
2026-03-02 22:15:54 +0900

    twofile1.cpp
    modify

--- source/Chapter09/ch09_07.cpp
+++ source/Chapter09/ch09_07.cpp
@@ -1,8 +1,8 @@
 // twofile1.cpp -- 외부 링크와 내부 링크를 가지는 변수
-#include <iostream>
-int tom = 3;
-int dick = 30;
-static int harry = 300;
+#include <iostream>							// towfile2.cpp와 함께 컴파일해야 한다
+int tom = 3;								// 외부 변수를 정의한다
+int dick = 30;								// 외부 변수를 정의한다
+static int harry = 300;						// 정적 변수, 내부 링크
 
 // 함수 원형
 void remote_access();
