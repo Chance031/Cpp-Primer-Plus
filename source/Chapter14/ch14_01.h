361e5282de67cf5e163258d51bb3e6001959bb40
찬스 <aday031@naver.com>
2026-03-23 23:19:47 +0900

    studentc.h
    modify

--- source/Chapter14/ch14_01.h
+++ source/Chapter14/ch14_01.h
@@ -19,7 +19,7 @@ public:
 	explicit Student(int n) : name("Nully"), scores(n) {}
 	Student(const std::string& s, int n) : name(s), scores(n) {}
 	Student(const std::string& s, const ArrayDb& a) : name(s), scores(a) {}
-	Student(const char& str, const double* pd, int n) : name(str), scores(pd, n) {}
+	Student(const char* str, const double* pd, int n) : name(str), scores(pd, n) {}
 	~Student() {}
 	double Average() const;
 	const std::string& Name() const;
